fc8f99184d958a42fcaface4df98e472ef3e38eb
Chris Mears <chris.mears@gmail.com>
2025-02-10 21:56:27 -0800

    [Feature] Add /meta endpoint

--- routes/api/v1/routes.php
+++ routes/api/v1/routes.php
@@ -4,12 +4,14 @@ use App\Http\Controllers\Api\V1\GetBrewery;
 use App\Http\Controllers\Api\V1\ListBreweries;
 use App\Http\Controllers\Api\V1\RandomBrewery;
 use App\Http\Controllers\Api\V1\SearchBreweries;
+use App\Http\Controllers\Api\V1\GetBreweriesMeta;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::get('/breweries', ListBreweries::class);
     Route::get('/breweries/random', RandomBrewery::class);
     Route::get('/breweries/search', SearchBreweries::class);
+    Route::get('/breweries/meta', GetBreweriesMeta::class);
 
     Route::middleware('cache.headers:public;max_age=300;etag')->group(function () {
         Route::get('/breweries/{id}', GetBrewery::class);
